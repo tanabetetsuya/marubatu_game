ef012aa480bad14fb7c19ef6e5a2ac2a5a9f8980
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-05 20:25:12 +0900

    [Update]

--- marubatu.rb
+++ marubatu.rb
@@ -16,33 +16,39 @@
       while true do
         puts "数字のマスを選んでください[1〜9]"
         s = gets.chomp.to_s
-        if s == "1" || s == "2" || s == "3" || s == "4" || s == "5"|| s == "6" || s == "7" || s == "8" || s == "9"
-          numbers.gsub!("#{s}", "○")
-          player.player_number << s
-          numbers_cp.delete(s)
+        if numbers_cp.include?(s)
+          if s == "1" || s == "2" || s == "3" || s == "4" || s == "5"|| s == "6" || s == "7" || s == "8" || s == "9"
+            numbers.gsub!("#{s}", "○")
+            player.player_number << s
+            numbers_cp.delete(s)
+            puts numbers
+            marubatu_judge(player, computer)
+          else
+            puts "無効な値です。もう一度入力してください。"
+          end
+
+          c = numbers_cp.sample
+          puts "computerは#{c}を選びました"
+          numbers.gsub!("#{c}", "×")
+          computer.computer_number << c
+          numbers_cp.delete(c)
           puts numbers
           marubatu_judge(player, computer)
+
         else
           puts "無効な値です。もう一度入力してください。"
         end
-
-        c = numbers_cp.sample
-        puts "computerは#{c}を選びました"
-        numbers.gsub!("#{c}", "×")
-        computer.computer_number << c
-        numbers_cp.delete(c)
-        puts numbers
-        marubatu_judge(player, computer)
       end
     end
+    
       #勝敗判定
       def marubatu_judge(player, computer)
-        xp = player.player_number
-        yc = computer.computer_number
-        if xp == ["1", "2", "3"] || xp == ["4", "5", "6"] || xp == ["7", "8", "9"] || xp == ["1", "4", "7"] || xp == ["2", "5", "8"] || xp == ["3", "6", "9"] || xp == ["1", "5", "9"] || xp == ["3", "5", "7"]
+        x = player.player_number
+        y = computer.computer_number
+        if x.include?("1") && x.include?("2") && x.include?("3") || x.include?("4") && x.include?("5") && x.include?("6") || x.include?("7") && x.include?("8") && x.include?("9") || x.include?("1") && x.include?("4") && x.include?("7") || x.include?("2") && x.include?("5") && x.include?("8") || x.include?("3") && x.include?("6") && x.include?("9") || x.include?("1") && x.include?("5") && x.include?("9") || x.include?("3") && x.include?("5") && x.include?("7")
           puts "あなたの勝ちです"
           exit
-        elsif yc == ["1", "2", "3"] || yc == ["4", "5", "6"] || yc == ["7", "8", "9"] || yc == ["1", "4", "7"] || yc == ["2", "5", "8"] || yc == ["3", "6", "9"] || yc == ["1", "5", "9"] || yc == ["3", "5", "7"]
+        elsif y.include?("1") && y.include?("2") && y.include?("3") || y.include?("4") && y.include?("5") && y.include?("6") || y.include?("7") && y.include?("8") && y.include?("9") || y.include?("1") && y.include?("4") && y.include?("7") || y.include?("2") && y.include?("5") && y.include?("8") || y.include?("3") && y.include?("6") && y.include?("9") || y.include?("1") && y.include?("5") && y.include?("9") || y.include?("3") && y.include?("5") && y.include?("7")
           puts "あなたの負けです"
           exit
         else
